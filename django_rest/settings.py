5200c3a2fceab84030be4d91bc677972e91b4b43
Milos Zivanovic <milos.zivanovic@msgconsulting.net>
2020-04-18 17:35:11 +0200

    setting default permission class to allow any request

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = '/static/'
 # Django Rest Framework Settings
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
